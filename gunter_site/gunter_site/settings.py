f616ddebc24bc485ed91f674ad3d4b04805d6e1f
Evgeniy <73117023+Jentos2020@users.noreply.github.com>
2023-01-23 10:50:43 +0530

    Update settings.py

--- gunter_site/gunter_site/settings.py
+++ gunter_site/gunter_site/settings.py
@@ -18,9 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u3i6kddcd*3o$&aor5po*&_9c*q8zl*#f0g3hd%-l&4mln30vy'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
